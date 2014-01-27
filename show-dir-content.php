9ab7ba7beda4a8680db2aa94bcafe16b40163cf8
Bojan <bojan92zm@yahoo.com>
2014-01-27 03:23:51 +0100

    third commit

--- show-dir-content.php
+++ show-dir-content.php
@@ -1,16 +1,23 @@
 <?php
+/** 
+* IPC_Test_Config 
+* 
+* PHP version 5 
+* 
+* @category Php
+* @package  MojPaket 
+* @author   Bojan  <bojan92zm@yahoo.com> 
+* @license  http://www.licenca.com/copyleft/licenca.html  Licenca 
+* @link     http://show-dir-content.com 
+*/
 echo "The current directory is ". getcwd() . ".";
 $scan = scandir(getcwd());
 echo "Its contents are:\n";
-foreach($scan as $v)
-{
-	if(($v == ".") || ($v == ".."))
-	{
-		echo("");
-	}
-	else
-	{
-		echo($v . "\n");
-	}
+foreach ($scan as $v) {
+    if (($v == ".") || ($v == "..")) {
+        echo("");
+    } else {
+        echo($v . "\n");
+    }
 }
 ?>
